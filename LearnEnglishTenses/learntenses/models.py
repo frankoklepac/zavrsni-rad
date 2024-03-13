899580e42d034527e871ba250a0b6eb96ec2d896
frankoklepac <itsmefranko1@gmail.com>
2024-03-13 15:49:13 +0100

    Add template

--- LearnEnglishTenses/learntenses/models.py
+++ LearnEnglishTenses/learntenses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
