58aeeadb8f408d7d81ad97cbdbcbe3794c202c06
frankoklepac <itsmefranko1@gmail.com>
2024-03-19 19:18:51 +0100

    Finished registration form

--- LearnEnglishTenses/learntenses/forms.py
+++ LearnEnglishTenses/learntenses/forms.py
@@ -1,26 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User as AuthUser 
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import UserProfile
 
-class UserRegisterForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+class UserRegisterForm(UserCreationForm):
+    name = forms.CharField(max_length=30)
+    age = forms.IntegerField()
 
     class Meta:
-        model = User
-        fields = ['name', 'username', 'age', 'email', 'password1', 'password2']
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+        model = AuthUser
+        fields = ['username', 'email', 'password1', 'password2', 'name', 'age']
 
     def save(self, commit=True):
         user = super(UserRegisterForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
+            UserProfile.objects.create(user=user, name=self.cleaned_data["name"], age=self.cleaned_data["age"])
         return user
