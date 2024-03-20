0249b7e8cd6e3031c59aa970d8c4f0afcd821ee7
frankoklepac <itsmefranko1@gmail.com>
2024-03-20 14:15:04 +0100

    Add profile, edit profile and change password urls

--- LearnEnglishTenses/learntenses/urls.py
+++ LearnEnglishTenses/learntenses/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('landing/', views.landing, name='landing'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
