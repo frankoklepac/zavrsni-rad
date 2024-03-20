a05bbd8e320d6b6c6e0c0daac8fe0ea6be6cd422
frankoklepac <itsmefranko1@gmail.com>
2024-03-20 14:14:41 +0100

    Add forms for changing profile and user details

--- LearnEnglishTenses/learntenses/forms.py
+++ LearnEnglishTenses/learntenses/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User as AuthUser 
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
 
@@ -8,7 +8,7 @@ class UserRegisterForm(UserCreationForm):
     age = forms.IntegerField()
 
     class Meta:
-        model = AuthUser
+        model = User
         fields = ['username', 'email', 'password1', 'password2', 'name', 'age']
 
     def save(self, commit=True):
@@ -18,3 +18,13 @@ class UserRegisterForm(UserCreationForm):
             user.save()
             UserProfile.objects.create(user=user, name=self.cleaned_data["name"], age=self.cleaned_data["age"])
         return user
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['name', 'age']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
