4b363485bbab5e010c5498b727e4970a6d39876f
frankoklepac <itsmefranko1@gmail.com>
2024-03-23 16:07:14 +0100

    Registered Task and UserTask in the admin panel

--- LearnEnglishTenses/learntenses/admin.py
+++ LearnEnglishTenses/learntenses/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Task, UserTask
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Task)
+admin.site.register(UserTask)
