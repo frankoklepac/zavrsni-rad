18f98ac113adea1ed37fc6601c277dc45dcbd978
frankoklepac <itsmefranko1@gmail.com>
2024-04-06 11:49:17 +0200

    Bug fix: JavaScript wasn't loading correctly, so the entire JavaScript code for the task_detail HTML has been moved to the HTML. Will try to fix this issue

--- LearnEnglishTenses/learntenses/static/learntenses/js/scripts.js
+++ LearnEnglishTenses/learntenses/static/learntenses/js/scripts.js
@@ -1,21 +1,2 @@
-let clickedWords = [];
-const selectedWordsElement = document.getElementById('selected-words');
-document.addEventListener('DOMContentLoaded', () => {
-
-  document.querySelectorAll('.word-button').forEach(button => {
-      button.addEventListener('click', () => {
-          let word = button.textContent;
-          let index = clickedWords.indexOf(word);
-          if (index === -1) {
-              clickedWords.push(word);
-          } else {
-              clickedWords.splice(index, 1);
-          }
-          selectedWordsElement.textContent = "Selected words:" + clickedWords.join(' ');
-          console.log(clickedWords); 
-      });
-  });
-
-  
-}
-});
+```Currently empty because of an error where the file is not being loaded correctly.```
+```Will try to fix this issue in the future.```
